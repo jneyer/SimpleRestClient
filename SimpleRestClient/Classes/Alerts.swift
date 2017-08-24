a40c12574e2cd6fe00886232fbdaf474a0756d23
John Neyer <jneyer@captechconsulting.com>
2017-08-24 13:33:28 -0400

    mapping

--- SimpleRestClient/Classes/Alerts.swift
+++ SimpleRestClient/Classes/Alerts.swift
@@ -1,5 +1,5 @@
 //
-//  Alerts.swift
+//  Alert.swift
 //  Pods
 //
 //  Created by John Neyer on 8/24/17.
@@ -10,15 +10,34 @@ import Foundation
 import ObjectMapper
 
 public class Alerts : Mappable {
-    var alerts: [Alert]?
-    var results: Int?
+    
+    var route_id : String?
+    var route_name : String?
+    var mode : String?
+    var advisory : Bool?
+    var detour : Bool?
+    var alert : Bool?
+    var suspended : Bool?
+    var last_updated : String?
+    var snow : Bool?
+    var description : String?
     
     required public init?(map: Map) {
+        
     }
     
     public func mapping(map: Map) {
-        alerts <- map["alerts"]
-        results <- map["results"]
+        
+        route_id <- map["route_id"]
+        route_name <- map["route_name"]
+        mode <- map["mode"]
+        advisory <- map["advisory"]
+        detour <- map["detour"]
+        alert <- map["alert"]
+        suspended <- map["suspended"]
+        last_updated <- map["last_updated"]
+        snow <- map["snow"]
+        description <- map["description"]
     }
     
 }
