f6adabe14a0cd122961adda84d0b5bc062e62d98
John Neyer <jneyer@captechconsulting.com>
2017-08-24 14:23:46 -0400

    Alert details

--- SimpleRestClient/Classes/AlertDetails.swift
+++ SimpleRestClient/Classes/AlertDetails.swift
@@ -9,6 +9,23 @@
 import Foundation
 import ObjectMapper
 
-public class AlertDetails {
+public class AlertDetails : Mappable {
     
+    var alert_id: String?
+    var alert: AlertDetails_Alert?
+    var last_updated: String?
+    var snow: Bool?
+    
+    required public init?(map: Map) {
+    }
+
+    public func mapping(map: Map) {
+        
+        alert_id <- map["alert_id"]
+        alert <- map["alert"]
+        last_updated <- map["last_updated"]
+        snow <- map["snow"]
+        
+    }
+
 }
