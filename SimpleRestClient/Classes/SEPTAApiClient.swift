07e3df6ce8dc9737eb5332d7cfdc800abfc25b43
John Neyer <jneyer@captechconsulting.com>
2017-08-25 08:57:44 -0400

    Adding SEPTA client wrapper

---
+++ SimpleRestClient/Classes/SEPTAApiClient.swift
@@ -0,0 +1,67 @@
+//
+//  SEPTAApiClient.swift
+//  Pods
+//
+//  Created by John Neyer on 8/25/17.
+//
+//
+
+import Foundation
+import PromiseKit
+
+public class SEPTAApiClient : SimpleRestClient {
+    
+    
+    public func getAlerts (route: String?) -> Promise<Alerts?> {
+        
+        var param : [String: String]?  = nil
+        if (route != nil) {
+            param = ["route": route!]
+        }
+        
+        return self.get(route: .Alerts, parameters: param! as [String : AnyObject])
+        
+    }
+    
+    public func getAlertDetails (routeName: String) -> Promise<AlertDetails?> {
+        
+        let param = ["route-name": routeName] as [String : AnyObject]
+        
+        return self.get(route: .AlertDetails, parameters: param )
+        
+    }
+    
+    public func getArrivals (origin: String, destination: String) -> Promise<Arrivals?> {
+        
+        let param = ["origin": origin, "destination" : destination] as [String : AnyObject]
+        
+        return self.get(route: .Arrivals, parameters: param)
+    }
+    
+    public func getRealTimeArrivals (originId: String, destinationId: String, transitType: TransitType, route: String?) -> Promise<RealTimeArrivals?> {
+        
+        let param = ["origin": originId, "destination" : destinationId, "type": transitType.value, "route": route] as [String : AnyObject]
+        
+        return self.get(route: .RealTimeArrivals, parameters: param)
+        
+    }
+    
+    public func getTransitRoutes (route: String) -> Promise<TransitRoutes?> {
+        
+        let param = ["route": route] as [String : AnyObject]
+        
+        return self.get(route: .TransitRoutes, parameters: param)
+        
+    }
+    
+    
+    public func getTrainRoutes (route: String) -> Promise<TrainRoutes?> {
+        
+        let param = ["route": route] as [String : AnyObject]
+        
+        return self.get(route: .TrainRoutes, parameters: param)
+        
+    }
+    
+    
+}
